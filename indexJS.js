05b9a4edfe3697a28d9e4ebe997a6dd0967c6dc1
gallagerWSCW <159972359+gallagerWSCW@users.noreply.github.com>
2026-08-13 16:17:57 +1200

    Addind a vertical navigation bar
    Addind a vertical navigation bar

--- indexJS.js
+++ indexJS.js
@@ -3,9 +3,24 @@ let lastScroll = 0;
 window.addEventListener("scroll", function () {
     if (window.scrollY > lastScroll) {
         document.getElementById("navbar").classList.add("hide");
+        document.getElementById("mobileMenuButton").classList.add("hide");
     } else {
         document.getElementById("navbar").classList.remove("hide");
+        document.getElementById("mobileMenuButton").classList.remove("hide");
     }
 
     lastScroll = window.scrollY;
 });
+
+
+const mobileMenuButton = document.getElementById("mobileMenuButton");
+const mobileMenu = document.getElementById("mobileMenu");
+
+
+mobileMenuButton.addEventListener("click", function () {
+
+    mobileMenu.classList.toggle("active");
+    mobileMenuButton.classList.toggle("active");
+
+});
+
